01f07864eccf3760888d0d9236486e4f242e3f90
Joao Vasconcelos <jvsn19@gmail.com>
2018-07-23 13:48:53 -0300

    lat long csv + map of marco zero

--- maps/script.js
+++ maps/script.js
@@ -1,4 +1,4 @@
-var mymap = L.map('mapid').setView([30.505, -0.09], 13);
+var mymap = L.map('mapid').setView([-8.0632, -34.8711], 13);
 
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -6,3 +6,9 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
     id: 'mapbox.streets',
     accessToken: 'pk.eyJ1IjoianZzbjE5IiwiYSI6ImNqanlnZ2ZleDAwbXIzd2xzOXdwb3pqeWoifQ.QIEWDvnKar46fdCIEJSvMg'
 }).addTo(mymap);
+
+L.marker([-8.0632, -34.8711]).addTo(mymap).bindPopup("Welcome to Marco Zero").openPopup();
+
+d3.csv("user_lat_long.csv", function(){
+    console.log(data[0]);
+});
