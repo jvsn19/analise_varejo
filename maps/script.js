72b5ae75fb8eb568a52603371c0fdf85b491eb82
Joao Vasconcelos <jvsn19@gmail.com>
2018-07-23 13:10:08 -0300

    starting maps

---
+++ maps/script.js
@@ -0,0 +1,8 @@
+var mymap = L.map('mapid').setView([30.505, -0.09], 13);
+
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox.streets',
+    accessToken: 'pk.eyJ1IjoianZzbjE5IiwiYSI6ImNqanlnZ2ZleDAwbXIzd2xzOXdwb3pqeWoifQ.QIEWDvnKar46fdCIEJSvMg'
+}).addTo(mymap);
